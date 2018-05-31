491726790572cc2866fd124fa214af8a269a5496
Paris Ward <wardparis.pw@gmail.com>
2018-05-30 23:31:01 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  postsCollection: AngularFirestoreCollection<Post>;
+  posts: any;
+
+  title: string;
+  content: string;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCollection = this.afs.collection('posts');
+    this.posts = this.postsCollection.valueChanges();
+  }
+
+  addPost() {
+    //this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+const firestoreConfig = {
+  apiKey: "AIzaSyDZka7b1ciMPFlgeuQkaIbu2j5_xCfNBi4",
+  authDomain: "firestore-tutorial-60649.firebaseapp.com",
+  databaseURL: "https://firestore-tutorial-60649.firebaseio.com",
+  projectId: "firestore-tutorial-60649",
+  storageBucket: "firestore-tutorial-60649.appspot.com",
+  messagingSenderId: "790963609161"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firestoreConfig),
+    AngularFirestoreModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
